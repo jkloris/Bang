6407aea16e59163434c7b09987f9a2609a1fe7f2
jkloris <kapusta.jergus@gmail.com>
2020-07-13 21:20:56 +0200

    add imgs, assets

---
+++ public/js/Assets.js
@@ -0,0 +1,31 @@
+let Sprites = {};
+let Notloaded_assets = 0;
+
+function AssetsLoading_loop(){
+
+    if(Notloaded_assets > 0){
+        requestAnimationFrame(AssetsLoading_loop.bind(this));
+    } 
+}
+
+function loadAssets(){
+
+    function loadIMG(path, imgName){
+        Notloaded_assets++;
+
+        var img = new Image();
+        img.src = path + imgName;
+
+        img.onload = function(){
+            Notloaded_assets--;
+        }
+
+        return img;
+    }
+   
+
+    Sprites.bang = loadIMG("imgs/", "bang.png");
+    Sprites.vedle = loadIMG("imgs/", "missed.png");
+
+    AssetsLoading_loop();
+}
---
+++ public/js/cards.js
@@ -0,0 +1,65 @@
+class Card {
+    constructor(){
+        this.name = null;
+        this.available = true;   
+    }
+    acion(){
+
+    }
+    draw(){
+
+    }
+}
+
+class BlueCard extends Card{
+    constructor(){
+        super();
+    }
+    effect(){
+
+    }
+}
+
+class ActionCard extends Card{
+    constructor(){
+        super();
+    }
+    postAction(){
+        //vyhod kartu
+    }
+}
+
+class Bang extends ActionCard{
+    constructor(){
+        super();
+        this.name = "Bang";
+    }
+    action(){
+        alert("bang");
+        // socket.emit("bang", );
+    }
+    draw = function(x, y, ratio){
+        // ctx.drawImage(Sprites.bang, x, y, ratio, Sprites.bang.height / Sprites.bang.width * ratio);
+        console.log(x,y,this);
+        
+    }
+}
+
+class Vedle extends ActionCard{
+    constructor(){
+        super();
+        this.name = "Vedle";
+    }
+    action(){
+        alert("vedle");
+        // socket.emit("", );
+    }
+    draw = function(x, y, ratio){
+        console.log(x,y,this);
+        // ctx.save();
+        // ctx.drawImage(Sprites.vedle, x, y, ratio, Sprites.bang.height / Sprites.bang.width * ratio);
+        // ctx.restore();
+    }
+}
+
+module.exports = [Bang, Vedle];

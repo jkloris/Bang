44fd07cf0912682c4892d3bc6ec7bd2e42769772
jkloris <kapusta.jergus@gmail.com>
2020-09-10 15:18:37 +0200

    zvuk piva a dynamitu

--- public/js/audio.js
+++ public/js/audio.js
@@ -7,3 +7,6 @@ audio.bang[1] = new Audio('../audio/bang1.mp3');
 audio.gulomet = new Audio('../audio/gulomet.mp3');
 // audio.indiani = new Audio('../audio/indiani.mp3');
 audio.indiani = new Audio('../audio/indiani2.mp3');
+
+audio.pivo = new Audio('../audio/pifko.mp3');
+audio.dynamit = new Audio('../audio/dynamit.mp3');

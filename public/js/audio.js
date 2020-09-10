e9fac512c1d2c233e885b8c5422341fcec839c40
jkloris <kapusta.jergus@gmail.com>
2020-09-10 14:56:14 +0200

    audio indiani v2

--- public/js/audio.js
+++ public/js/audio.js
@@ -5,4 +5,5 @@ audio.bang[0] = new Audio('../audio/bang0.mp3');
 audio.bang[1] = new Audio('../audio/bang1.mp3');
 
 audio.gulomet = new Audio('../audio/gulomet.mp3');
-audio.indiani = new Audio('../audio/indiani.mp3');
+// audio.indiani = new Audio('../audio/indiani.mp3');
+audio.indiani = new Audio('../audio/indiani2.mp3');
